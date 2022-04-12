3f49ba1a7176074be8b200743263365370180b24
frozar <fabien.rozar@gmail.com>
2022-04-12 15:49:27 +0400

    Add redirection from index to upload image page.

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
  */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('image-upload');
 });
 
 Route::get('image-upload', [ImageController::class, 'index']);
